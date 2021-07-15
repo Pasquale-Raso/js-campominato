92ad39db91c56b48e1a960f7b90d65323824a484
Pasquale-Raso <82545398+Pasquale-Raso@users.noreply.github.com>
2021-07-15 17:23:35 +0200

    modifiche

--- js/javascript.js
+++ js/javascript.js
@@ -6,31 +6,84 @@
 // Se il numero scelto dall'utente è presente tra i numeri bomba, la partita termina.
 // Quando la partita termina, comunichiamo all'utente il suo punteggio.
 
-//1.  creo array per bombe e genero 16 numeri random
-//2.  creo una variante di appoggio dei numeri Inseriti Dall'Utente
-//3.  creo una ciclo for che per 16 volte chiede un numero alla volta e confronta se è presente nell'array delle bombe
-//4.  creo un variabile contatore che se il numero digitato non è presente nell'array bombe, aggiunge +1
-//
+// ____________________________________________PREPARAZIONE________________________________________________________________
+var totalNumbers = 5;
+var totalBombs = 2;
+var possibilita = totalNumbers - totalBombs;
+var numeriBombe = [1, 2];
 
-// ___________________creo array per bombe e genero 16 numeri random_________________________
+// while (numeriBombe.length < totalBombs) {
+//   var numeroCasuale = getRandomNumber(1, totalNumbers);
 
-var Bombe = [];
-while (Bombe.length < 16) {
-  var random = Math.floor(Math.random() * 100 + 1);
-  if (!Bombe.includes(random)) Bombe.push(random);
-}
-console.log(Bombe);
+//   //---------------------------------------- o cosi-------------------------------
+//   // while (numeriBombe.length < totalBombs) {
+//   //   var numeroCasuale = Math.floor(Math.random() * (totalNumber - 1 + 1) + 1);
 
-// ___creo una variante di appoggio dei numeri Inseriti Dall'Utente_________________________
+//   if (!isInArray(numeroCasuale, numeriBombe)) {
+//     numeriBombe.push(numeroCasuale);
+//   }
+//   //---------------------------------------- o cosi-------------------------------
+//   // if (!numeriBombe.includes(numeroCasuale)) {
+//   //   numeriBombe.push(numeroCasuale);
+//   // }
+// }
 
-var numeriInseritiDaUtente = 0;
+// _______________________________________________GIOCO__________________________________________________________________
 
-// creo una ciclo for che per 16 volte chieda un numero allavolta e confronta se è presente nell'array delle bombe_________________________
-for (let i = 0; i < 3; i++) {
-  var numeriInseritiDaUtente = prompt("Inserisci un numero da 1 a 100", "5");
-  if (numeriInseritiDaUtente === Bombe[0]) {
-    alert("BOOM! HAI PERSO!");
-  } else {
-    alert("BENE! CONTINUA A GIOCARE!");
+var numeriUtente = [];
+
+// while (numeriUtente.length < possibilita) {
+//   var userChoice = getUserNumber(1, totalNumbers);
+// }
+
+var userChoice = prompt("inserisci un numero da 1 a " + totalNumbers);
+
+while (numeriUtente.length < possibilita) {
+  while (
+    !userChoice ||
+    userChoice.trim() === "" ||
+    isNaN(userChoice) ||
+    userChoice < 1 ||
+    userChoice > totalNumbers
+  ) {
+    userChoice = prompt("inserisci un numero da 1 a " + totalNumbers);
   }
+  userChoice = parseInt(userChoice);
 }
+
+// _____________________________________________FUNZIONE_________________________________________________________________
+
+// genera un numero tra il minimo e il massimo
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+// controlla che un dato elemento sia presente in un ARRAY
+function isInArray(needle, arr) {
+  var found = false;
+  var i = 0;
+  while (!found && i < arr.length) {
+    if (needle === arr[i]) {
+      found = true;
+    }
+    i++;
+  }
+  return found;
+}
+
+// controlla i numeri inseriti dall'utente
+
+// function getUserNumber(min, max) {
+//   var number;
+//   do {
+//     number = prompt("inserisci un numero da " + min + " a " + max);
+//   } while (
+//     !number ||
+//     number.trim() === "" ||
+//     isNaN(number) ||
+//     number < min ||
+//     number > max
+//   );
+//   return parseInt(number);
+// }
